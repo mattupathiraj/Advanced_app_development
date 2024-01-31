196f8edb3fa1da78d1a6c6f1b947b2bd84b76ff2
mattupathiraj <119093454+mattupathiraj@users.noreply.github.com>
2024-01-31 00:13:26 -0500

    Add files via upload

---
+++ front-end/Day3/Componenets/Events.jsx
@@ -0,0 +1,46 @@
+
+
+const Events = () => {
+  return (
+    <div className="event-container"><br></br>
+<center><h2>Wedding Event</h2></center><br></br>
+      <center><img src="https://www.bca.co.id/-/media/Feature/News/Edukatips/2022/09/20220912-tips-nonton-konser-pertama-kali-banner.jpg" alt="Event" className="event-image" /></center>
+      <center><div className="event-description">bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</div></center>
+<br></br><br></br><br></br>
+      <center><h2>Wedding Event</h2></center><br></br>
+      <center><img src="https://cdn0.weddingwire.com/vendor/136011/3_2/960/jpg/inside-crest-center_51_110631.jpeg" alt="Event" className="event-image" /></center>
+      <center><div className="event-description">bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</div></center>
+      <br></br><br></br><br></br>
+      <center><h2>Wedding Event</h2></center><br></br>
+      <center><img src="https://cdn0.weddingwire.com/vendor/046811/3_2/960/jpg/new-65_51_118640-1567136310.jpeg" alt="Event" className="event-image" /></center>
+      <center><div className="event-description">bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</div></center>
+<br></br><br></br><br></br>
+      <center><h2>Wedding Event</h2></center><br></br>
+      <center><img src="https://vyasthitevents.com/wp-content/uploads/2023/06/marriage-events.jpg" alt="Event" className="event-image" /></center>
+      <center><div className="event-description">bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</div></center>
+      <br></br><br></br><br></br>
+      <center><h2>Wedding Event</h2></center><br></br>
+      <center><img src="https://cdn0.weddingwire.com/vendor/136011/3_2/960/jpg/inside-crest-center_51_110631.jpeg" alt="Event" className="event-image" /></center>
+      <center><img src="" alt="Event" className="event-image" /></center>
+      <center><div className="event-description">bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</div></center>
+<br></br><br></br><br></br>
+      <center><h2>Wedding Event</h2></center><br></br>
+      <center><img src="https://cdn0.weddingwire.com/vendor/111300/3_2/960/jpg/1522430812-af3d117fba2b7dbf-jessie-betts-127.jpeg" alt="Event" className="event-image" /></center>
+      <center><div className="event-description">bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</div></center>
+    </div>
+  )
+}
+
+export default Events
